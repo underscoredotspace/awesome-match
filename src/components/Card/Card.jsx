dd52be84b84374aafc2ee5094e3e67aac85068fc
Colin Tindle <ampersand@underscore.space>
2018-10-10 22:38:13 +0100

    chore: prettier first run

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import "./Card.scss";
+import React from 'react'
+import './Card.scss'
 
 const Card = ({ icon, rotated }) => (
-  <div className={`card${rotated ? " rotate" : ""}`}>
+  <div className={`card${rotated ? ' rotate' : ''}`}>
     <div className="front" />
     <div className={`back fa fa-${icon}`} />
   </div>
-);
+)
 
-export default Card;
+export default Card
